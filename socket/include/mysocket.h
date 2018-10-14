ca34da080044a6f38d81bfbcc04a569ae4d1ca46
lqinggang <1944058861@qq.com>
2018-10-14 05:07:09 -0700

    add include/mysocket.h

---
+++ socket/include/mysocket.h
@@ -0,0 +1,18 @@
+/****************************************
+   > File Name: mysocket.h
+   > Author: lqinggang
+   > Email: 1944058861@qq.com
+   > Create Time: Sun 14 Oct 2018 04:38:55 AM PDT
+****************************************/
+
+#ifndef MYSOCKET_H
+#define MYSOCKET_H
+
+#define serverIP         127.0.0.1
+#define serverPort       12345
+#define clientPort       12346
+
+#define BUFFSIZE         4096 
+#define MAXBACKLOG       16
+
+#endif
